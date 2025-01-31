a5798cd0017ca7b45434ccf8b76d9d8eee08d060
Naman Katare <naman.12323443@lpu.in>
2025-01-31 21:29:15 +0530

    done solution 4

--- solution4.java
+++ solution4.java
@@ -1,18 +1,22 @@
 public class solution4 {
     public static void main(String[] args){
         java.util.Scanner sc = new java.util.Scanner(System.in);
-        System.out.println("Enter the amount: ");
         int a = sc.nextInt();
-        if(a>=5000){
-            float b  = a*20/100;
-            System.out.printf("Discount: %.2f\n",b);
-            System.out.println("Final Amount: " + (a-b));
-
-        }
-        else if(a>=3000  && a<5000){
-            float c = a*10/100;
-            System.out.printf("Discount: %.2f\n",c);
-            System.out.println("Final Amount: " + (a-c));
+        System.out.println("Enter age: "+a);
+        int b = sc.nextInt();
+        System.out.println("Enter Salary: " + b);
+        boolean c = sc.nextBoolean();
+        System.out.println("Any due: " + c);
+        if(a>=18){
+            if(b>=30000){
+                if(c == false){
+                    System.out.println("You are eligible for premium membership"); //                              
+                }else{
+                    System.out.println("Not Eligible for Membership");
+                }
+            }
+        }else{
+            System.out.println("Not eligible");
         }
     }
 }
