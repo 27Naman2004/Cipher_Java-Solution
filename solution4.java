e61fd7114091831344e1c2be2bc75a24a5d94b89
Naman Katare <naman.12323443@lpu.in>
2025-01-31 12:51:08 +0530

    New

---
+++ solution4.java
@@ -0,0 +1,23 @@
+public class solution4 {
+    public static void main(String[] args){
+        java.util.Scanner sc = new java.util.Scanner(System.in);
+        System.out.println("Enter the amount: ");
+        int a = sc.nextInt();
+        if(a>=5000){
+            float b  = a*20/100;
+            System.out.printf("Discount: %.2f\n",b);
+            System.out.println("Final Amount: " + (a-b));
+
+        }
+        else if(a>=3000  && a<5000){
+            float c = a*10/100;
+            System.out.printf("Discount: %.2f\n",c);
+            System.out.println("Final Amount: " + (a-c));
+        }
+        else{
+            System.out.println("No Discount\n");
+            System.out.println("Final Amount: " + a);
+
+        }
+    }
+}
