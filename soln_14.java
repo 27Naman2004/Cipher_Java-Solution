138069e8b4c8ed833677d9733604dcb68342cb4a
Naman Katare <naman.12323443@lpu.in>
2025-02-06 18:38:08 +0530

    changes made

---
+++ soln_14.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class soln_14 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+
+        for(int i=0;i<=a;i++){
+            if(i%3==0){
+                System.out.println("Fizz\n");
+            }else if(i%5==0){
+                System.out.println("Buzz\n");
+            }else if(i%3==0 && i%5==0){
+                System.out.println("FizzBuzz\n");
+            }
+            else{
+                System.out.println(i+"\n");
+            }
+
+        }
+
+        
+        
+        scanner.close();
+    }
+}
