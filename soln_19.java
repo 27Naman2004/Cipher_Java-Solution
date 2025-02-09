270daca1929eca584f06b7cd66164520417b5d56
Naman Katare <naman.12323443@lpu.in>
2025-02-09 19:19:38 +0530

    changes made

--- soln_19.java
+++ soln_19.java
@@ -1,23 +1,32 @@
-import java.util.Scanner;
-public class soln_16 {
+
+import java.util.*;
+public class soln_19 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         
         int n = scanner.nextInt();
         int [] arr = new int[n];
+
         for(int i=0;i<n;i++){
             arr[i] = scanner.nextInt();
         }
-        // 2nd largest element
-        int a = arr[0];
-        for (int i = 0; i < n; i++){
-            if (arr[i] > a){
-                a = arr[i];
+        Arrays.sort(arr);
+        int largest = Integer.MIN_VALUE, secondLargest = Integer.MIN_VALUE;
+
+        for (int num : arr) {
+            if (num > largest) {
+                secondLargest = largest;
+                largest = num;
+            } else if (num > secondLargest && num != largest) {
+                secondLargest = num;
             }
         }
-        
 
-        System.out.println("Second Largest number is: " + b);
+        if (secondLargest == Integer.MIN_VALUE) {
+            System.out.println("Not Found.");
+        } else {
+            System.out.println(" " + secondLargest);
+        }
         scanner.close();
     }
 }
--- tempCodeRunnerFile.java
+++ tempCodeRunnerFile.java
@@ -1,1 +1,19 @@
-int
+
+import java.util.*;
+public class soln_19 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        int n = scanner.nextInt();
+        int [] arr = new int[n];
+
+        for(int i=0;i<n;i++){
+            arr[i] = scanner.nextInt();
+        }
+        Arrays.sort(arr);
+        System.out.println(arr[n-1]);
+        
+        
+        scanner.close();
+    }
+}
