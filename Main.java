57703420e660ed6bdb0c76a03c78dd99d90048de
Naman Katare <naman.12323443@lpu.in>
2025-02-10 19:08:22 +0530

    changes made

---
+++ Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        int n = scanner.nextInt();
+        int b = 0;
+        while(n!=0){
+            int c = n%10;
+            b = b*10 + c;
+            n = n/10; 
+        }
+        System.out.println(b);
+        
+        
+        scanner.close();
+    }
+}
